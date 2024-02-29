18255b882c765ba6df22814c20860a60f7a35eb4
JB <joseph.barnett@gmail.com>
2024-02-29 14:16:41 -0300

    update package name

--- exec.ps1
+++ exec.ps1
@@ -1,2 +1,2 @@
-docker build --rm -t auth0-vue-01-login .
-docker run -p 3000:3000 --pid=host auth0-vue-01-login
+docker build --rm -t codeblind.ai/app .
+docker run -p 3000:3000 --pid=host codeblind.ai/app
